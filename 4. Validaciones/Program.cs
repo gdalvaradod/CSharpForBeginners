fc5e2279c1a93f788c5624dbed3555b19efc6be8
Geovanny De Jesús Alvarado Dzul <geovanny.alvarado@softtekprojects.com>
2023-05-30 08:24:09 -0600

    Ciclos/Validaciones added

--- 4. Validaciones/Program.cs
+++ 4. Validaciones/Program.cs
@@ -1,1 +1,39 @@
-﻿
+﻿// Validaciones o Condiciones
+// If statements
+
+/*
+Sentencias más utilizadas
+a < b - Menor que
+a > b - Mayor que
+a <= b - Menor que o igual
+a >= b - Mayor que o igual
+a == b - Igual
+a != b - Diferente de
+
+Condiciones
+if
+else
+else if
+*/
+
+// Declaración de variables
+int edad = 16;
+string password = "Dante2023!!!!!!";
+string passwordOld = "Dante2023!";
+
+if(edad >= 18){
+    Console.WriteLine("Puede tener licencia para conducir");
+}
+else{
+    Console.WriteLine("Es menor de edad");
+}
+
+if(password == "Joshua2023!"){
+    Console.WriteLine("Password aceptado");
+}
+else if(password == passwordOld){
+    Console.WriteLine("Estás utilizando un password anterior");
+}
+else{
+    Console.WriteLine("La contraseña es incorrecta");
+}
---
+++ 5. Ciclos/Program.cs
@@ -0,0 +1,28 @@
+﻿/*
+Ciclos o loops
+
+For
+While
+*/
+
+/*
+For
+
+for(variable de referencia; condición a cumplir; incremento de variable de referencia)
+*/
+for(int i = 0; i < 5; i++){
+    Console.WriteLine(i + 1);
+}
+
+/*
+While
+
+while(condición){}
+*/
+int a = 0;
+
+while(a < 5){
+    Console.WriteLine(a + 1);
+    a++;
+}
+
