a1f4a0fbec6bdc9713dafba7ec310910cb9c8b4b
Geovanny De Jesús Alvarado Dzul <geovanny.alvarado@softtekprojects.com>
2023-06-11 16:34:15 -0600

    Nuevas clases y ejercicios agregados

---
+++ 6. Switch/Program.cs
@@ -0,0 +1,26 @@
+﻿Console.WriteLine("Introduce el número del día");
+var dia = Console.ReadLine();
+switch (Int32.Parse(dia)) 
+{
+  case 1:
+    Console.WriteLine("Lunes");
+    break;
+  case 2:
+    Console.WriteLine("Martes");
+    break;
+  case 3:
+    Console.WriteLine("Miércoles");
+    break;
+  case 4:
+    Console.WriteLine("Jueves");
+    break;
+  case 5:
+    Console.WriteLine("Viernes");
+    break;
+  case 6:
+    Console.WriteLine("Sábado");
+    break;
+  case 7:
+    Console.WriteLine("Domingo");
+    break;
+}
