1e7a92254c5379507218ff4534e24287295b57f4
Geovanny De Jesús Alvarado Dzul <geovanny.alvarado@softtekprojects.com>
2023-06-12 06:47:07 -0600

    ejercicio de tablas agregado

---
+++ 0. Ejercicios/Tablas de multiplicar/Program.cs
@@ -0,0 +1,72 @@
+﻿Console.WriteLine("¿Qué tabla de multiplicar quieres imprimir?");
+var tabla = Console.ReadLine();
+int result = 0;
+
+switch(Int32.Parse(tabla)){
+    case 1:
+        for(int i = 1; i <= 10; i++){
+            result = Int32.Parse(tabla) * i;
+            Console.WriteLine(Int32.Parse(tabla) + " X " + i + " = " + result);
+        }
+        break;
+
+    case 2:
+        for(int i = 1; i <= 10; i++){
+            result = Int32.Parse(tabla) * i;
+            Console.WriteLine(Int32.Parse(tabla) + " X " + i + " = " + result);
+        }
+        break;
+
+    case 3:
+        for(int i = 1; i <= 10; i++){
+            result = Int32.Parse(tabla) * i;
+            Console.WriteLine(Int32.Parse(tabla) + " X " + i + " = " + result);
+        }
+        break;
+
+    case 4:
+        for(int i = 1; i <= 10; i++){
+            result = Int32.Parse(tabla) * i;
+            Console.WriteLine(Int32.Parse(tabla) + " X " + i + " = " + result);
+        }
+        break;
+
+    case 5:
+        for(int i = 1; i <= 10; i++){
+            result = Int32.Parse(tabla) * i;
+            Console.WriteLine(Int32.Parse(tabla) + " X " + i + " = " + result);
+        }
+        break;
+
+    case 6:
+        for(int i = 1; i <= 10; i++){
+            result = Int32.Parse(tabla) * i;
+            Console.WriteLine(Int32.Parse(tabla) + " X " + i + " = " + result);
+        }
+        break;
+
+    case 7:
+        for(int i = 1; i <= 10; i++){
+            result = Int32.Parse(tabla) * i;
+            Console.WriteLine(Int32.Parse(tabla) + " X " + i + " = " + result);
+        }
+        break;
+
+    case 8:
+        for(int i = 1; i <= 10; i++){
+            result = Int32.Parse(tabla) * i;
+            Console.WriteLine(Int32.Parse(tabla) + " X " + i + " = " + result);
+        }
+        break;
+
+    case 9:
+        for(int i = 1; i <= 10; i++){
+            result = Int32.Parse(tabla) * i;
+            Console.WriteLine(Int32.Parse(tabla) + " X " + i + " = " + result);
+        }
+        break;
+
+    default:
+        Console.WriteLine("No entiendo tu respuesta");
+        break;
+}
