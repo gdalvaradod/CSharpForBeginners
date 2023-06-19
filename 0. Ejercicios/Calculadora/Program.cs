dc31024255c83114aa673bbbbafaac5ac86bc0b6
Geovanny De Jesús Alvarado Dzul <geovanny.alvarado@softtekprojects.com>
2023-06-19 08:06:56 -0600

    Agregada carpeta de listas

--- 0. Ejercicios/Calculadora/Program.cs
+++ 0. Ejercicios/Calculadora/Program.cs
@@ -1,45 +1,56 @@
 ﻿Console.WriteLine("Bienvenido a mi calculadora");
 
-// Solicitamos los números
-Console.WriteLine("Introduce el primer número");
-var a = Console.ReadLine();
-Console.WriteLine("Introduce el segundo número");
-var b = Console.ReadLine();
+// Variable de repetición
+int repetir = 1;
 
-// Menú
-Console.WriteLine("1. Suma");
-Console.WriteLine("2. Resta");
-Console.WriteLine("3. Multiplicación");
-Console.WriteLine("4. División");
-Console.WriteLine("===============================");
+while(repetir == 1){
+    
+    // Solicitamos los números
+    Console.WriteLine("Introduce el primer número");
+    var a = Console.ReadLine();
+    Console.WriteLine("Introduce el segundo número");
+    var b = Console.ReadLine();
 
-// Solicitamos la operación a realizar
-Console.WriteLine("¿Qué operación quieres realizar?");
-var opcion = Console.ReadLine();
+    // Menú
+    Console.WriteLine("1. Suma");
+    Console.WriteLine("2. Resta");
+    Console.WriteLine("3. Multiplicación");
+    Console.WriteLine("4. División");
+    Console.WriteLine("===============================");
 
-// Variable de resultado
-double resultado = 0.0;
+    // Solicitamos la operación a realizar
+    Console.WriteLine("¿Qué operación quieres realizar?");
+    var opcion = Console.ReadLine();
 
-// Lógica del menú
-switch(Int32.Parse(opcion)){
-    case 1: // Suma
-        resultado = (double) Int32.Parse(a) + Int32.Parse(b);
-        Console.WriteLine(a + " + " + b + " = " + resultado);
-        break;
-    case 2: // Resta
-        resultado = (double) Int32.Parse(a) - Int32.Parse(b);
-        Console.WriteLine(a + " - " + b + " = " + resultado);
-        break;
-    case 3: // Multiplicación
-        resultado = (double) Int32.Parse(a) * Int32.Parse(b);
-        Console.WriteLine(a + " X " + b + " = " + resultado);
-        break;
-    case 4: // División
-        resultado = (double) Int32.Parse(a) / Int32.Parse(b);
-        //double resultado = Convert.toDouble(a) / Convert.toDouble(b);
-        Console.WriteLine(a + " / " + b + " = " + resultado);
-        break;
-    default:
-        Console.WriteLine("Opción no válida");
-        break;
+    // Declaración de variables
+    double resultado = 0.0;
+
+    // Lógica del menú
+    switch(Int32.Parse(opcion)){
+        case 1: // Suma
+            resultado = (double) Int32.Parse(a) + Int32.Parse(b);
+            Console.WriteLine(a + " + " + b + " = " + resultado);
+            break;
+        case 2: // Resta
+            resultado = (double) Int32.Parse(a) - Int32.Parse(b);
+            Console.WriteLine(a + " - " + b + " = " + resultado);
+            break;
+        case 3: // Multiplicación
+            resultado = (double) Int32.Parse(a) * Int32.Parse(b);
+            Console.WriteLine(a + " X " + b + " = " + resultado);
+            break;
+        case 4: // División
+            resultado = (double) Int32.Parse(a) / Int32.Parse(b);
+            //double resultado = Convert.toDouble(a) / Convert.toDouble(b);
+            Console.WriteLine(a + " / " + b + " = " + resultado);
+            break;
+        default:
+            Console.WriteLine("Opción no válida");
+            break;
+    }
+
+    Console.WriteLine("¿Desea continuar?");
+    int respuesta = Int32.Parse(Console.ReadLine());
+    repetir = respuesta;
 }
+
---
+++ 8. Listas/Program.cs
@@ -0,0 +1,55 @@
+﻿List<int> MisNumeros = new List<int>(); // Lista de enteros o números
+// Agregar elementos a nuestra listaa
+MisNumeros.Add(1);
+MisNumeros.Add(2);
+MisNumeros.Add(3);
+MisNumeros.Add(4);
+MisNumeros.Add(5);
+
+/*
+Lista de manera lógica
+[]  ---> [1,2,3,4,5]
+[]  ---> [0,1,2,3,4]
+*/
+
+List<string> MisCompras = new List<string>(); // Lista de cadena o string
+// Agregar elementos a nuestra listaa
+MisCompras.Add("Huevos");
+MisCompras.Add("Mantequilla");
+MisCompras.Add("Aceite");
+MisCompras.Add("Leche");
+MisCompras.Add("Cereal");
+
+var Alumnos = new List<string>()
+{
+    "Alejandro",
+    "Luis",
+    "Jimmer",
+    "Mario",
+    "Manuel"
+};
+
+Console.WriteLine("============================");
+Console.WriteLine("Lista original");
+Console.WriteLine("============================");
+for(int i = 0; i < Alumnos.Count; i++){
+    Console.WriteLine(i + ". " + Alumnos[i]);
+}
+
+Console.WriteLine("============================");
+Console.WriteLine("Lista con valores nuevos");
+Console.WriteLine("============================");
+Alumnos.Add("Joshua");
+Alumnos.Add("Alan");
+for(int i = 0; i < Alumnos.Count; i++){
+    Console.WriteLine(i + ". " + Alumnos[i]);
+}
+
+Console.WriteLine("============================");
+Console.WriteLine("Lista con nombres removidos");
+Console.WriteLine("============================");
+Alumnos.RemoveAt(2);
+Alumnos.RemoveAt(1);
+for(int i = 0; i < Alumnos.Count; i++){
+    Console.WriteLine(i + ". " + Alumnos[i]);
+}
